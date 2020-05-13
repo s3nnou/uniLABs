7e5ddd15b3b818b96bc9ca0081582a3c4f88a282
Uladlen <s3nnou@gmail.com>
2020-05-13 09:21:31 +0300

    Добавьте файлы проекта.

---
+++ lab4/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace lab4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.dirView = new System.Windows.Forms.TreeView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtShow = new System.Windows.Forms.ListBox();
+            this.dirItemsList = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.button2);
+            this.splitContainer1.Panel1.Controls.Add(this.textBox1);
+            this.splitContainer1.Panel1.Controls.Add(this.button1);
+            this.splitContainer1.Panel1.Controls.Add(this.dirView);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.button3);
+            this.splitContainer1.Panel2.Controls.Add(this.textBox2);
+            this.splitContainer1.Panel2.Controls.Add(this.label3);
+            this.splitContainer1.Panel2.Controls.Add(this.label2);
+            this.splitContainer1.Panel2.Controls.Add(this.txtShow);
+            this.splitContainer1.Panel2.Controls.Add(this.dirItemsList);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 450);
+            this.splitContainer1.SplitterDistance = 266;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // dirView
+            // 
+            this.dirView.Location = new System.Drawing.Point(12, 28);
+            this.dirView.Name = "dirView";
+            this.dirView.Size = new System.Drawing.Size(251, 240);
+            this.dirView.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(26, 294);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(211, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "New Folder Name";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(26, 360);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(211, 34);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Delete Folder";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(26, 320);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(211, 34);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Create Folder";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtShow
+            // 
+            this.txtShow.FormattingEnabled = true;
+            this.txtShow.Location = new System.Drawing.Point(133, 28);
+            this.txtShow.Name = "txtShow";
+            this.txtShow.Size = new System.Drawing.Size(385, 342);
+            this.txtShow.TabIndex = 1;
+            this.txtShow.Visible = false;
+            // 
+            // dirItemsList
+            // 
+            this.dirItemsList.HideSelection = false;
+            this.dirItemsList.Location = new System.Drawing.Point(3, 28);
+            this.dirItemsList.MultiSelect = false;
+            this.dirItemsList.Name = "dirItemsList";
+            this.dirItemsList.Size = new System.Drawing.Size(124, 342);
+            this.dirItemsList.TabIndex = 0;
+            this.dirItemsList.UseCompatibleStateImageBehavior = false;
+            this.dirItemsList.View = System.Windows.Forms.View.List;
+            this.dirItemsList.ItemActivate += new System.EventHandler(this.listView1_ItemActivate);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Directory Viewer";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "File Viewer";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(133, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 15);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Text Preview";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(3, 384);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(345, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(354, 376);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(153, 34);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Open parent folder";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        public System.Windows.Forms.TreeView dirView ;
+        private System.Windows.Forms.ListView dirItemsList;
+        private System.Windows.Forms.ListBox txtShow;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ lab4/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+
+namespace lab4
+{
+    public partial class Form1 : Form
+    {
+        private string parentFolderPath;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.dirView.NodeMouseClick += new TreeNodeMouseClickEventHandler(this.treeView1_NodeMouseClick);
+        }
+
+        private void PopulateTreeView()
+        {
+                MonitorDirectory(parentFolderPath);
+                TreeNode rootNode;
+
+                DirectoryInfo info = new DirectoryInfo(parentFolderPath);
+
+                if (info.Exists)
+                {
+                    rootNode = new TreeNode(info.Name);
+                    rootNode.Tag = info;
+                    GetDirectories(info.GetDirectories(), rootNode);
+                    dirView.Nodes.Add(rootNode);
+                }
+        }
+
+        private void GetDirectories(DirectoryInfo[] subDirs, TreeNode nodeToAddTo)
+        {
+            TreeNode aNode;
+            DirectoryInfo[] subSubDirs;
+            foreach (DirectoryInfo subDir in subDirs)
+            {
+                aNode = new TreeNode(subDir.Name, 0, 0);
+                aNode.Tag = subDir;
+                subSubDirs = subDir.GetDirectories();
+                if (subSubDirs.Length != 0)
+                {
+                    GetDirectories(subSubDirs, aNode);
+                }
+                nodeToAddTo.Nodes.Add(aNode);
+            }
+        }
+
+        private void treeView1_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            TreeNode newSelected = e.Node;
+            dirItemsList.Items.Clear();
+            DirectoryInfo nodeDirInfo = (DirectoryInfo)newSelected.Tag;
+            ListViewItem item = null;
+
+            foreach (FileInfo file in nodeDirInfo.GetFiles())
+            {
+                item = new ListViewItem(file.FullName, 1);
+                if(item.ToString().Contains(".txt"))
+                    dirItemsList.Items.Add(item);
+            }
+
+            dirItemsList.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+        }
+
+        private void listView1_ItemActivate(object sender, EventArgs e)
+        {
+            txtShow.Show();
+            string[] lines = File.ReadAllLines(dirItemsList.SelectedItems[0].Text);
+
+            foreach (string item in lines)
+            {
+                txtShow.Items.Add(item);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dirView.BeginUpdate();
+            TreeNode node = dirView.SelectedNode;
+            
+            string path = null;
+            if (node.Parent != null)
+            {
+                Object anode = node.Tag;
+                path = node.Parent.Tag.ToString() + "\\" + dirView.SelectedNode.Tag.ToString();
+            }
+            else
+            {
+                Object aNode = dirView.SelectedNode.Tag;
+                path = aNode.ToString();
+            }
+
+            string path1 = Path.Combine(path, textBox1.Text);
+            Directory.CreateDirectory(path1);
+            dirView.Nodes.Clear();
+
+            PopulateTreeView();
+            dirView.EndUpdate();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dirView.BeginUpdate();
+
+            TreeNode node = dirView.SelectedNode;
+
+            string path = null;
+
+            if (node.Parent != null)
+            {
+                Object anode = node.Tag;
+                path = node.Parent.Tag.ToString() + "\\" + dirView.SelectedNode.Tag.ToString();
+            }
+            else
+            {
+                Object aNode = dirView.SelectedNode.Tag;
+                path = aNode.ToString();
+            }
+
+            System.IO.DirectoryInfo di = new DirectoryInfo(path);
+
+            foreach (FileInfo file in di.GetFiles())
+            {
+                file.Delete();
+            }
+            foreach (DirectoryInfo dir in di.GetDirectories())
+            {
+                dir.Delete(true);
+            }
+
+            Directory.Delete(path);
+
+            dirView.Nodes.Clear();
+            PopulateTreeView();
+
+            dirView.EndUpdate();
+        }
+
+        private static void MonitorDirectory(string path)
+        {
+            FileSystemWatcher fileSystemWatcher = new FileSystemWatcher();
+
+            fileSystemWatcher.Path = path;
+
+            fileSystemWatcher.Renamed += FileSystemWatcher_Renamed;
+
+            fileSystemWatcher.IncludeSubdirectories = true;
+            fileSystemWatcher.EnableRaisingEvents = true;
+
+
+        }
+
+        private static void FileSystemWatcher_Renamed(object sender, FileSystemEventArgs e)
+        {
+            MessageBox.Show(text: $"File: {e.Name} renamed to {e.FullPath}");
+            
+        }  
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+
+            DialogResult result = dialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                textBox2.Text = dialog.SelectedPath;
+                parentFolderPath = dialog.SelectedPath;
+            }
+
+            PopulateTreeView();
+        }
+    }
+}
